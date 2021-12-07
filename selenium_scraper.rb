f500d9c8608affa1c2c3591a1a3845714ebd5f2d
Rasel <nextcoming17@gmail.com>
2021-12-07 18:12:30 +0600

    Added new script to get total available tickets from scraping, added selenium implemtation

--- selenium_scraper.rb
+++ selenium_scraper.rb
@@ -7,32 +7,33 @@ require 'pry'
 # driver = Selenium::WebDriver.for(:firefox, options: options)
 
 driver = Selenium::WebDriver.for :firefox
-#Generate the search url physically using any date, time and put here, we will make it dynamic later
+
+# driver.navigate.to "https://www.tour.ne.jp/j_air/list/?adult=1&air_type=2&arr_out=ITM&change_date_in=0&change_date_out=0&date_out=20211221&dpt_out=TYO"
 driver.navigate.to "https://www.tour.ne.jp/j_air/list/?adult=1&air_type=2&arr_out=ITM&change_date_in=0&change_date_out=0&date_out=20211231&dpt_out=TYO&time_from_out=0600&time_to_out=0700&time_type_out=0"
+wait = Selenium::WebDriver::Wait.new(:timeout => 10000)
 
 #Take some time to load the page
-# sleep(5)
+sleep(10)
 
 ticket_summary_button = wait.until {
   elements = driver.find_element(:css, "#Act_Airline_Out")
 }
 
+sleep(10)
+
 ticket_summary_button.click
 
-#Take some time after click on second tab to load result html
-# sleep(5)
+sleep(5)
 
-#Find available information and search companies name, this is optional
 ticket_summary = driver.find_elements(:class, "airline-name")
-
-#Find available ticke count element
 ticket_available_lists = driver.find_elements(:class, "toggle-btn-company")
 
-#Find each companies available ticket and sum to get total available tickets
-total_available_ticket = 0
-ticket_available_lists.each do |ticket_count|
-    total_available_ticket += ticket_count.text.delete("^0-9").to_i
-end
+# binding.pry
 
-puts "Available ticket companies name = " + ticket_summary.first.text + ", " + ticket_summary.last.text
-puts "Total available ticket found is = " + total_available_ticket.to_s
+available_ticket = 0
+
+ticket_available_lists.each do |ticket_count|
+    available_ticket += ticket_count.text.delete("^0-9").to_i
+end
+puts "available ticket companies = " + ticket_summary.first.text + ", " + ticket_summary.last.text
+puts "Total available ticket found is = " + available_ticket.to_s
