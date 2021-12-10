9cce4ede34a1a95ec8235738a4a35e3bd5d61447
Rasel <nextcoming17@gmail.com>
2021-12-10 19:27:35 +0600

    Tour site scraper updated for round trip ticket search

--- tour_site_scraper.rb
+++ tour_site_scraper.rb
@@ -3,43 +3,46 @@ require 'selenium-webdriver'
 require 'pry'
 
 MAX_RETRY = 100
-# driver = Selenium::WebDriver.for :firefox
-# profile = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
-# capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(firefox_profile: profile)
-# driver = Selenium::WebDriver.for :Remote, desired_capabilities: capabilities
-# client = Selenium::WebDriver::Remote::Http::Default.new
-# client.read_timeout = 120 # seconds
-# driver = Selenium::WebDriver.for :firefox, http_client: client
-# client = Selenium::WebDriver::Remote::Http::Default.new
-# client.proxy = Selenium::Proxy.new(http: "proxy.org:8080")
+WAIT = Selenium::WebDriver::Wait.new(timeout: 20)
 
+# options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
+# driver = Selenium::WebDriver.for(:firefox, options: options)
 driver = Selenium::WebDriver.for :firefox
 
 puts 'Trying to fetch data from site.....'
 puts '--------------------------------------------------------'
 
+def check_return_tickets_visibility(driver)
+  begin
+    # Wait for few seconds until able to find return tickets list
+    WAIT.until { driver.find_element(css: "#Act_response_in .toggle-btn-company").displayed? }
+    WAIT.until { driver.find_element(css: "#Act_response_in .airline-name").displayed? }
+  rescue Exception
+  end
+end
+
 ticket_search_date_from = Date.new(2021, 12, 31)
 ticket_search_date_to = Date.new(2022, 01, 31)
-
 ticket_search_date_from.upto(ticket_search_date_to) do |dt|
-  departure_date = dt.to_s.delete("-")
+  departure_date_in = dt.to_s.delete("-")
+  departure_date_out = dt.to_s.delete("-")
+
   puts "\n\nTickets for this date " + dt.to_s
 
-  # This options are for headless execution of the browser so that it don't need to load browser
-  # options = Selenium::WebDriver::Firefox::Options.new(args: ['-headless'])
-  # driver = Selenium::WebDriver.for(:firefox, options: options)
-
   # Generate the search url physically using any date, time and put here, we will make it dynamic later based on requirement
-  driver.navigate.to 'https://www.tour.ne.jp/j_air/list/?adult=1&air_type=2&arr_out=ITM&change_date_in=0&change_date_out=0&date_out='+ departure_date + '&dpt_out=TYO&time_from_out=0600&time_to_out=0700&time_type_out=0'
-  # sleep(1)  # Wait 1s to load the page properly
-
-  # MAX_RETRY = 100
+  driver.navigate.to 'https://www.tour.ne.jp/j_air/list/?adult=1&arr_in=TYO&arr_out=CTS&change_date_in=0&change_date_out=0&date_in=' + departure_date_in + '&date_out=' + departure_date_out + '&dpt_in=CTS&dpt_out=TYO&time_from_out=0600&time_to_out=0700&time_type_out=0'
+  sleep(1)  # Wait 1s to load the page properly
   begin
     retries ||= 0
-    ticket_summary_button = nil
-    ticket_summary_button = driver.find_element(:css, '#Act_Airline_Out')
-    return if ticket_summary_button.nil?
-    ticket_summary_button.click
+    ticket_summary_button_out = nil
+    ticket_summary_button_out = driver.find_element(:css, '#Act_Airline_Out')
+    ticket_summary_button_in = driver.find_element(:css, '#Act_Airline_In')
+    return if ticket_summary_button_out.nil? && ticket_summary_button_in.nil?
+    ticket_summary_button_out.click
+    ticket_summary_button_in.click
+
+    WAIT.until { driver.find_element(css: "#Act_response_out .toggle-btn-company").displayed? }
+    WAIT.until { driver.find_element(css: "#Act_response_out .airline-name").displayed? }
   rescue Exception => e
     puts 'Trying to fetch data.. ' + retries.to_s
     retries += 1
@@ -48,24 +51,42 @@ ticket_search_date_from.upto(ticket_search_date_to) do |dt|
     raise "Could not get ticket website information: Please give necessary information to search"
   end
 
-  wait = Selenium::WebDriver::Wait.new(timeout: 20)
-  wait.until { driver.find_element(class: "toggle-btn-company").displayed? }
-  wait.until { driver.find_element(class: "airline-name").displayed? }
+  check_return_tickets_visibility(driver)
 
-  # Find available information and available ticket list elements
-  ticket_summary = driver.find_elements(:class, 'airline-name')
-  ticket_available_lists = driver.find_elements(:class, 'toggle-btn-company')
+  # Scrap Available Tickets Elements
+  ticket_summary = driver.find_elements(:css, '#Act_response_out .airline-name')
+  ticket_available_lists = driver.find_elements(:css, '#Act_response_out .toggle-btn-company')
 
-  # Parse elements to find each companies available ticket and sum to get total available tickets
+  # Parse elements to find each companies available ticket and sum
   total_available_ticket = 0
   !ticket_available_lists.nil? && ticket_available_lists.each do |ticket_count|
     total_available_ticket += ticket_count.text.delete('^0-9').to_i
   end
 
-  puts 'Available ticket companies name = '
+  # Scrap Returning Tickets Elements
+  ticket_summary_in = driver.find_elements(:css, '#Act_response_in .airline-name')
+  ticket_available_lists_in = driver.find_elements(:css, '#Act_response_in .toggle-btn-company')
+
+  # Parse elements to find each companies returning tickets and sum
+  total_available_ticket_in = 0
+  !ticket_available_lists_in.nil? && ticket_available_lists_in.each do |ticket_count_in|
+    total_available_ticket_in += ticket_count_in.text.delete('^0-9').to_i
+  end
+
+  # Write all tickets search results
+  puts 'Total available ticket OUT found is = ' + total_available_ticket.to_s
+  puts 'Total available ticket IN found is = ' + total_available_ticket_in.to_s
+
+  puts 'Available ticket IN companies name : '
+  puts '------------------------------------'
+  !ticket_summary_in.nil? && ticket_summary_in.each do |ticket_cmpany_in|
+    puts ticket_cmpany_in.text.to_s + ', '
+  end
+
+  puts
+  puts 'Available ticket OUT companies name : '
+  puts '-------------------------------------'
   !ticket_summary.nil? && ticket_summary.each do |ticket_cmpany|
     puts ticket_cmpany.text.to_s + ', '
   end
-  puts 'Total available ticket found is = ' + total_available_ticket.to_s
-
 end
