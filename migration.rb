442d72d48eeb4f6dba2491231e022ee274a77144
Rasel <nextcoming17@gmail.com>
2021-12-15 16:12:55 +0600

    Migration file added, ticket company table insert query updated

---
+++ migration.rb
@@ -0,0 +1,41 @@
+require 'sqlite3'
+    
+db = SQLite3::Database.open "db_tour_scraper.db"
+
+db.execute <<SQL
+CREATE TABLE IF NOT EXISTS tickets_summary (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  departure_date Date,
+  return_date Date,
+  time_from_out VARCHAR(20),
+  time_to_out VARCHAR(20),
+  search_time DateTime,
+  total_tickets_out INTEGER,
+  total_tickets_in INTEGER
+);
+SQL
+
+db.execute <<SQL
+CREATE TABLE IF NOT EXISTS tickets_airline_companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ticket_summary_id INTEGER,
+  airline_company_name VARCHAR(100),
+  ticket_lowest_price FLOAT,
+  total_flights_available INTEGER,
+  ticket_type VARCHAR(10),
+  FOREIGN KEY(ticket_summary_id) REFERENCES tickets_summary(id)
+);
+SQL
+
+db.execute <<SQL
+CREATE TABLE IF NOT EXISTS airline_flights (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ticket_airline_id INTEGER,
+  flight_code VARCHAR(50),
+  flight_price INTEGER,
+  flight_changable_status VARCHAR(50),
+  flight_ticket_type VARCHAR(100),
+  FOREIGN KEY(ticket_airline_id) REFERENCES tickets_airlines(id)
+);
+SQL
+    
